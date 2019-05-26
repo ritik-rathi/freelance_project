7cdd0f5837e5c0124ca41bd159a3276228b23e53
Aayush Malhotra <37381075+AadumKhor@users.noreply.github.com>
2019-05-26 19:26:07 +0530

    Added temporary splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:freelance/login.dart';
 import 'package:freelance/guard_screens/delivery_details.dart';
 import 'package:freelance/guard_screens/maidRec.dart';
 import 'package:freelance/guard_screens/mainScreen.dart';
 import 'package:freelance/guard_screens/new_visitor.dart';
+import 'package:freelance/splashscreen.dart';
 import 'package:freelance/user_screens/profile.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -15,12 +20,12 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'HOMANTRA',
       theme: ThemeData(fontFamily: 'Raleway', primaryColor: Color(0xFF50CDFF)),
-      home: Profile(),
+      home: SplashScreen(),
       routes: {
         '/newvisitor': (context) => NewVisitorPage(),
         '/user': (context) => Profile(),
         '/guard': (context) => MainScreen(),
-        '/delivery':(context) => DeliveryPage(),
+        '/delivery': (context) => DeliveryPage(),
         '/maidrec': (context) => MaidRec()
       },
     );
---
+++ lib/splashscreen.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(Duration(seconds: 2),
+        () => Navigator.popAndPushNamed(context, '/guard'));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      color: Color(0xFF50CDFF),
+      height: MediaQuery.of(context).size.height,
+      child: Center(
+        child: Image.asset('assets/images/logo.jpeg' , fit: BoxFit.scaleDown,width: 200.0,height: 200.0,),
+      ),
+    );
+  }
+}
