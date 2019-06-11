b29be2557485c8911495bcefaacbfbd98972f355
Aayush Malhotra <37381075+AadumKhor@users.noreply.github.com>
2019-06-11 16:19:47 +0530

    OTP working

--- lib/guard_screens/new_visitor.dart
+++ lib/guard_screens/new_visitor.dart
@@ -4,24 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:freelance/guard_screens/otp.dart';
 import 'package:image_picker/image_picker.dart';
 
-import 'dart:math';
-import 'package:http/http.dart' as http;
-
-int otp = Random().nextInt(99999);
-
-var apiKey = "u89iYz/h3dU-3Ab3nrQBqIYMbe8KSzowDCSTslyGNM";
-//from the acc that I am using
-var message = "Your OTP is $otp"; //vary otp using dart
-var sender = "TXTLCL"; //default sender, can be changed
-var numbers = "919953610152"; //coz test
-
-var url =
-    'https://api.textlocal.in/send/?+apiKey=$apiKey&message=$message&sender=$sender&numbers=$numbers';
-
-var response = http.get(url).then((res) {
-  print(res);
-});
-
 String vName, vPhone, vPurpose, vFlat, oName;
 String vTime = TimeOfDay.now().toString();
 
