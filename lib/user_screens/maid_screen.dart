dd3dfdfbb1759c9b8b6a709c2b5a9f8fe6312257
Aayush Malhotra <37381075+AadumKhor@users.noreply.github.com>
2019-07-29 16:21:45 +0530

    Maid bug solved

--- lib/user_screens/maid_screen.dart
+++ lib/user_screens/maid_screen.dart
@@ -54,7 +54,6 @@ class MaidScreen extends StatelessWidget {
             } else {
               continue;
             }
-            else continue;
           }
           print("Assigned data : ${mvm.name}");
           return mvm ?? null;
